19bf0cdff2c7040a23fe00309fc370f832f6449b
larisa <laurabrovkina@gmail.com>
2023-08-02 13:20:03 +1200

    Add the first repository test GetAll_ReturnsAllUsers

---
+++ IntegrationTests/RepositoryTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Model;
+using Repository;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests
+{
+    public class RepositoryTests
+    {
+        [Fact]
+        public async Task GetAll_ReturnsAllUsers()
+        {
+            // Arrange
+            var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=EfExpenseDemo;Integrated Security=True";
+            var databaseContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(connectionString);
+            var databaseContext = new ApplicationDbContext(databaseContextOptions.Options);
+            var userRepository = new UserRepository(databaseContext);
+            var expectedUsers = await databaseContext.Users.ToListAsync();
+
+            // Act
+            var actualUsers = userRepository.GetAll();
+            var result = actualUsers.OfType<User>().ToList();
+
+            // Assert
+            Assert.Equal(expectedUsers.Count, result.Count);
+            Assert.Equal(expectedUsers.First().UserId, actualUsers.First().UserId);
+            Assert.Equal(expectedUsers.Last().UserId, actualUsers.Last().UserId);
+        }
+    }
+}
